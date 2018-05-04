667e95d7137409694c206fc57f1930983fcbf98a
Eric Tapia <tapia.ea85@gmail.com>
2018-05-04 14:55:58 -0700

    First commit

---
+++ client/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+<!--	
+	<img src="./assets/logo.png">
+    <router-view/>
+-->
+
+  <Testvar></Testvar>
+  
+  </div>
+</template>
+
+
+
+
+
+
+
+<script>
+import Testvar from './components/TestComponent'
+
+export default {
+  name: 'App',
+  components: {
+  	Testvar
+  }
+}
+</script>
+
+
+
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/TestComponent.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="mytestclass">
+		
+		<h1>This is a test</h1>
+		
+		<h1>{{message1}}</h1>
+		
+		<p>{{user.lastName}}</p>
+	</div>
+
+</template>
+
+
+
+
+
+
+
+
+<script>
+	export default {
+		name: 'testname',
+		data() {
+			return {
+				message1: 'hello world message',
+				//objects
+				user: {
+					firstName: 'Eric',
+					lastName: 'Tapia' 
+				}
+			}
+		}
+	}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style scoped>
+
+</style>
